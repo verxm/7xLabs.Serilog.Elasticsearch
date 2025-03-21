9eda7567c4aa91ee7a64cbf58b1b9b75327020c6
Davi Santos <davisantos448@gmail.com>
2025-03-21 09:43:22 -0300

    style(TenantLogEnricher): break line between contructor and keys definition.

--- Serilog.Elk.POC/Enrichers/TenantLogEnricher.cs
+++ Serilog.Elk.POC/Enrichers/TenantLogEnricher.cs
@@ -7,9 +7,8 @@ namespace Serilog.Elk.POC.Enrichers
         private const string LOG_PROPERTY_NAME = "ZTenantId";
         private const string TENANT_ID_HEADER_KEY = "TenantId";
 
-        public TenantLogEnricher(ITenantHeaderAccessor tenantHeaderAccessor) : base(LOG_PROPERTY_NAME, TENANT_ID_HEADER_KEY, tenantHeaderAccessor)
-        {
-
-        }
+        public TenantLogEnricher(ITenantHeaderAccessor tenantHeaderAccessor) 
+            : base(LOG_PROPERTY_NAME, TENANT_ID_HEADER_KEY, tenantHeaderAccessor)
+        { }
     }
 }
